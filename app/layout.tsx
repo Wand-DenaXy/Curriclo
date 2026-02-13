bc9e3ef1f9c708d50d4323231062f146a231023e
Wand-DenaXy <manelocassil69@gmail.com>
2026-02-13 19:43:19 +0000

    Alterações

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
        <head>
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
       </head>
